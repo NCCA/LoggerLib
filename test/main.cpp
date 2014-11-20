525fc911ab341116adc3dccce96010444539b22b
jmacey <jmacey@bournemouth.ac.uk>
2014-11-20 22:15:22 +0000

    added a threaded demo but threading doesn't work yet

--- test/main.cpp
+++ test/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <cstdlib>
 #include "Logger.h"
+#include <pthread.h>
 
+void doThreadTest();
 
 int main()
 {
@@ -21,4 +23,74 @@ int main()
   log2.logMessage("this is log 2\n");
   for(int i=0; i<20; ++i)
     log2.logError("loop %d\n",i);
+  //doThreadTest();
 }
+nccalog::NCCALogger glog("thread.log");
+
+
+char *sharedMem;
+const static int SIZE=20;
+
+void *starFillerThread(void *arg)
+{
+	while(1)
+	{
+	glog.logMessage("Star Filler\n");
+	for(int i=0; i<SIZE; ++i)
+		sharedMem[i]='*';
+	sleep(1);
+	}
+}
+
+void *hashFillerThread(void *arg)
+{
+	while(1)
+	{
+	glog.logMessage("hash filler\n");
+	for(int i=0; i<SIZE; ++i)
+		sharedMem[i]='#';
+	sleep(1);
+	}
+}
+
+
+void *consumerThread(void *arg)
+{
+		while(1)
+		{
+		glog.logMessage("Consumer\n");
+		for(int i=0; i<SIZE; ++i)
+			glog.logMessage("%c",sharedMem[i]);
+		glog.logMessage("\n");
+		sleep(1);
+		}
+}
+
+void doThreadTest()
+{
+	glog.disableColours();
+	glog.disableLineNumbers();
+	glog.disableTimeStamp();
+	sharedMem = new char[SIZE];
+	pthread_t threadID[3];
+
+	pthread_create(&threadID[0],0,starFillerThread,0);
+	pthread_create(&threadID[1],0,hashFillerThread,0);
+	pthread_create(&threadID[2],0,consumerThread,0);
+
+	pthread_join(threadID[0],0);
+	pthread_join(threadID[1],0);
+	pthread_join(threadID[2],0);
+	int i=0;
+
+	while(i<200)
+	{
+		++i;
+		glog.logWarning("main thread %d\n",i);
+	}
+
+
+}
+
+
+
